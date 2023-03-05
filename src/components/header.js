24c1359c1abd942557133ba0a2c27b2b78f69790
Michael Beasley <michael@upscalepixels.com>
2023-03-05 11:36:10 -0500

    Reset back to a minimial design starting point

--- src/components/header.js
+++ src/components/header.js
@@ -3,8 +3,6 @@ import { Link } from "gatsby"
 
 import "../styles/header.css"
 
-import heroImage from "../images/penguin-header.png"
-
 const Header = ({ siteTitle }) => {
   const menuLinks = [
     {
@@ -31,35 +29,24 @@ const Header = ({ siteTitle }) => {
     <header>
       <nav>
         <div className="nav-container container">
-          <div className="menu-left"></div>
-          <div className="menu-middle">
-            <div className="inner-nav-container">
-              <h2>Site Logo</h2>
-              <div className="menu-links">
-                {menuLinks.map(link => (
-                  <Link to={link.url} key={link.url}>{link.text}</Link>
-                ))}
-              </div>
-              <div className="menu-toggle" aria-hidden="true" onClick={() => setIsMenuOpen(!isMenuOpen)}>
-                <span className="material-symbols-outlined">&#xe5d2;</span>
-              </div>              
+            <h2>Site Logo</h2>
+            <div className="menu-links">
+              {menuLinks.map(link => (
+                <Link to={link.url} key={link.url}>{link.text}</Link>
+              ))}
             </div>
-          </div>  
-          <div className="menu-right"></div>      
+            <div className="menu-toggle" aria-hidden="true" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+              <span className="material-symbols-outlined">&#xe5d2;</span>
+            </div>              
         </div>
-        <div className={isMenuOpen ? "mobile-menu expanded" : "mobile-menu"}>
-          <div className="mobile-menu-links">
+
+          <div className={isMenuOpen ? "mobile-menu-links expanded" : "mobile-menu-links"}>
             {menuLinks.map(link => (
               <Link to={link.url} key={link.url}>{link.text}</Link>
             ))}
           </div>             
-        </div>       
+   
       </nav>
-      <div className="hero-container container">
-        <div className="hero-img">
-          <img src={heroImage} alt="penguin in a top hat on a laptop" />
-        </div>        
-      </div>
     </header>    
   )
 }
