a1a25651c1c1493c772ba82789b73ad53b285b47
Michael Beasley <michael@upscalepixels.com>
2023-03-06 15:44:28 -0500

    query for blog posts started

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import * as React from "react"
 
 import Layout from "../components/layout"
+import Projects from "../components/projects"
 import Seo from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <h2>Main Content</h2>
+    <Projects />
   </Layout>
 )
 
